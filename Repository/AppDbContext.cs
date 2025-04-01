5146f5391b994ec640d718b7b9997ea97b4abdbf
Eren Nokta <109761393+Erenkta@users.noreply.github.com>
2025-04-01 11:31:45 +0300

    Db Context , Product Entity and Config Created

---
+++ Repository/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace App.Repositories
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options) // bu yapıya primary constructor denir.
+    {
+        public DbSet<Product> Products { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) // Configurasyon işlemleri
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); // IEntityTypeConfiguration'ı implemente eden her class'ı ekle
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
