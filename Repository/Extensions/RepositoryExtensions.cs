18210dd464dae1d5f397f0303fbbd82a8a1a437c
Eren Nokta <109761393+Erenkta@users.noreply.github.com>
2025-04-01 12:15:59 +0300

    Init migration added , Db Context Configured with options pattern

---
+++ Repository/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace App.Repositories.Extensions
+{
+    /// <summary>
+    ///     This class is used to add a new extension method to <typeparamref name="IServiceCollection" />.    
+    /// </summary>
+    public static class RepositoryExtensions
+    {
+        /// <summary>
+        ///     Adds a new method to <typeparamref name="IServiceCollection" /> for configuring the DbContext.
+        /// </summary>
+        public static IServiceCollection AddRepository(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                // Configuration üzerinden ConnectionStrings'i oku daha sonra da bunu ConnectionStringOption olarak belirttiğimiz class'a Cast et diyoruz.
+                var connectionStrings = configuration.GetSection(ConnectionStringOption.Key).Get<ConnectionStringOption>();
+                // burada connectionString!. ifadesi connectionString'in kesinlikle null gelmeyeceğini belirtir.
+                options.UseSqlServer(connectionStrings!.SqlServer, sqlServerOptionsAction =>
+                {
+                    //Migrations klasörünün hangi projede oluşturulacağını belirtir.
+                    sqlServerOptionsAction.MigrationsAssembly(typeof(RepositoryAssembly).Assembly.FullName);
+                });
+            });
+            return services; 
+        }
+    }
+}
